d88693a028f5d6a7a868d10907829e2731654aca
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-17 11:43:12 +0900

    Update PlayerBullet.java

--- ShootingGame/src/PlayerBullet.java
+++ ShootingGame/src/PlayerBullet.java
@@ -2,4 +2,11 @@ public class PlayerBullet extends Character{
     public PlayerBullet(double x, double y, double vx, double vy) {
         super(x, y, vx, vy);
     }
+
+    public void draw(MyFrame f) {
+        f.setColor(116,80,48);
+        f.fillOval(x,y+20,30,10);
+        f.fillOval(x+2.5,y+15,25,10);
+        f.fillOval(x+5,y+10,20,20);
+    }
 }
