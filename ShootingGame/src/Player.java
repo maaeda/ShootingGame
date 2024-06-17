eb1184b78192448fe8dddc623051367330a97b06
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-17 11:47:21 +0900

    Update Player.java

--- ShootingGame/src/Player.java
+++ ShootingGame/src/Player.java
@@ -29,6 +29,8 @@ public class Player extends Character implements KeyListener {
         }
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             GameWorld.playerBullets.add(new PlayerBullet(x,y,2,-10));
+            GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+            GameWorld.playerBullets.add(new PlayerBullet(x,y,-2,-10));
             System.out.println("玉の数="+GameWorld.playerBullets.size());
         }
     }
